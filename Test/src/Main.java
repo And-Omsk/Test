81ed617e6ec0b76700eb2ec49761bbdb50ed2dcc
And-Omsk <belik_om@mail.ru>
2024-06-21 10:42:04 +0600

    Неважно

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
             for (int i = 1; i <= 5; i++) {
                 System.out.println("--------------- I=" + i);
                 System.out.println("Сложение " + calk.slogenie(2.3, i));
-                //DDDDDDDDDDDDDDDDDDDDDDDDDDDD
+                //ggggggggggggggjhhf
                 System.out.println("Разность " + calk.raznost(15., i));
                 calk.delenie(12, i);
                 calk.umnog(2, i); // jhhghgjkhkhk
